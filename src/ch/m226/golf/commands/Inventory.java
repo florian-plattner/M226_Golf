cf4e3b1dcea6e1ada7cf443aff978abf51ad4841
flo <florian.plattner500@gmail.com>
2017-02-04 16:37:00 +0100

    Updated Description. Changed the inventory command, so it also prints the equipped weapon.

--- src/ch/m226/golf/commands/Inventory.java
+++ src/ch/m226/golf/commands/Inventory.java
@@ -9,6 +9,9 @@ import ch.m226.golf.items.Item;
 public class Inventory implements Command {
     @Override
     public boolean use(Game game, String[] args) {
+        System.out.println("Equipped: " +
+                (game.player.equippedWeapon != null ? game.player.equippedWeapon.name : "null"));
+
         for(Item item : game.player.inventory){
             System.out.println(item.name);
         }
